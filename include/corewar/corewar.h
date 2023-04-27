01d7136345e50d9a7a2432f65763d8db65f7eb3d
Antoine <antoine.le@epitech.eu>
2023-04-27 02:58:35 +0200

    :fix: makefile

---
+++ include/corewar/corewar.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** Corewar
+** File description:
+** corewar
+*/
+
+#ifndef COREWAR_H_
+    #define COREWAR_H_
+    #include "op.h"
+    #define ERROR 84
+    #define SUCESS 0
+
+void launch_parser(int ac, char **argv);
+
+#endif /* !COREWAR_H_ */
