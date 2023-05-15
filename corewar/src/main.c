7e15701edec85cb94e38ae7b227fc82da1010e48
LeTamanoir <martin.saldinger@gmail.com>
2023-05-15 22:57:29 +0200

    style: :art: code review

--- corewar/src/main.c
+++ corewar/src/main.c
@@ -8,26 +8,35 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "my_vec.h"
 
+#include "corewar/arguments.h"
 #include "corewar/corewar.h"
+
+static void free_arguments(arguments_t *args)
+{
+    for (size_t i = 0; i < args->programs->size; i++) {
+        free(args->programs->data[i].path);
+        close(args->programs->data[i].fd);
+    }
+    free(args->programs);
+}
 
 int main(int ac, char **av)
 {
     arguments_t args;
 
-    if (ac < 2)
+    if (ac < MIN_AC || (ac > MIN_AC && parse_cycles(av, &args)))
         return ERROR;
-    if (parse_cycles(av, &args))
-        return ERROR;
+
     args.programs = vec_create(100, sizeof(prog_t));
-    for (int i = 3; i < ac; i++) {
-        if (parse_prog(av, ac, &args, &i) == ERROR)
+
+    for (int i = START_AV; i < ac; i++)
+        if (parse_prog(av, ac, &args, &i) == false)
             return ERROR;
-    }
-    for (size_t i = 0; i < args.programs->size; i++)
-        free(args.programs->data[i].path);
-    free(args.programs);
+
+    free_arguments(&args);
     return SUCCESS;
 }
--- include/corewar/corewar.h
+++ include/corewar/corewar.h
@@ -7,10 +7,18 @@
 
 #ifndef COREWAR_H_
     #define COREWAR_H_
-    #define ERROR 84
-    #define SUCCESS 0
+    #define ERROR (84)
+    #define SUCCESS (0)
+
+    #define MIN_AC (2)
+    #define START_AV (3)
+
+    #define MAX_PROG_AV (5)
+
+    #include <stdbool.h>
+
     #include "corewar/arguments.h"
 
-int parse_cycles(char **av, arguments_t *args);
-int parse_prog(char **av, int ac, arguments_t *args, int *i);
+bool parse_cycles(char **av, arguments_t *args);
+bool parse_prog(char **av, int ac, arguments_t *args, int *i);
 #endif /* !COREWAR_H_ */
