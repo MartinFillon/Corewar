5350ebf9fa84b23064a97e73456751efddcbb946
LeTamanoir <martin.saldinger@gmail.com>
2023-05-18 15:21:55 +0200

    fix: :bug: fix bug in my_swap + add assertion to detect swap over `MAX_SWAP_SIZE`

--- lib/my_stdlib/my_swap.c
+++ lib/my_stdlib/my_swap.c
@@ -5,14 +5,15 @@
 ** generic swap
 */
 
+#include <assert.h>
+
 #include "my_stdlib.h"
 
 void my_swap(void *a, void *b, size_t elem_size)
 {
     static char temp[MAX_SWAP_SIZE] = {0};
 
-    if (elem_size > MAX_SWAP_SIZE)
-        return;
+    assert(elem_size < MAX_SWAP_SIZE);
 
     my_memcpy(temp, b, elem_size);
     my_memcpy(b, a, elem_size);
