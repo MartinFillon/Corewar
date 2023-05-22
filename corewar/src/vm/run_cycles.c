3ca31b3da3aae7e022dc692a06cff3172e95345a
Martin Saldinger <51637671+LeTamanoir@users.noreply.github.com>
2023-05-22 18:38:17 +0200

    69 vm cycles (#79)
    * refactor: :art: improve argv parsing + check for champion max size
    
    * 38 instructions from vm (#75)
    
    * fix: :bug: crash on empty labels (#74)
    
    * fix: :zap: prog size and error handling specification
    
    * fix: :bug: empty label crash
    
    fixed with no return 84
    
    * fix: :fire: and was shitty coded
    
    * feat: :sparkles: add all executors -> fork and lfork to be done and need to verify everything when subject will be re available
    
    * feat: :lipstick: coding-style
    
    * fix: :lipstick: style
    
    ---------
    
    Co-authored-by: lou <109749395+louonezime@users.noreply.github.com>
    
    * feat: :construction: vm work in progress
    
    * feat: :sparkles: vm core loop
    
    ---------
    
    Co-authored-by: MartinFillon <114775771+MartinFillon@users.noreply.github.com>
    Co-authored-by: lou <109749395+louonezime@users.noreply.github.com>

--- corewar/src/vm/run_cycles.c
+++ corewar/src/vm/run_cycles.c
@@ -5,58 +5,40 @@
 ** run cycles
 */
 
-#include <stdio.h>
+#include "my_stdio.h"
+
 #include "corewar/arguments.h"
 #include "corewar/corewar.h"
 #include "corewar/op.h"
 
-static u_char get_next_instruction(vm_t *vm, program_t *program)
-{
-    u_char next_instruction_idx = vm->arena[program->pc];
-
-    if (next_instruction_idx < 1 || next_instruction_idx > 16)
-        next_instruction_idx = INVALID_INSTRUCTION;
-
-    return next_instruction_idx;
-}
-
-void run_cycle(vm_t *vm)
-{
-    program_t *program = NULL;
-    u_char instruction_idx = INVALID_INSTRUCTION;
-
-    for (size_t i = 0; i < vm->programs->size; ++i) {
-        program = &vm->programs->data[i].program;
-
-        if (program->cycle_to_wait > 0) {
-            program->cycle_to_wait--;
-            continue;
-        }
-
-        if (instruction_idx != INVALID_INSTRUCTION) {
-            op_tab[instruction_idx].func(vm, program);
-        }
-
-        instruction_idx = get_next_instruction(vm, program);
-
-        program->cycle_to_wait = op_tab[instruction_idx].nbr_cycles;
-    }
-}
-
-void check_alive(vm_t *vm)
+static void check_alive(vm_t *vm)
 {
     for (size_t i = 0; i < vm->programs->size; ++i) {
         if (vm->programs->data[i].program.is_alive == false) {
-            vec_remove(vm->programs, i);
-            i--;
+            vm->programs->data[i].is_running = false;
         }
 
         vm->programs->data[i].program.is_alive = false;
     }
+}
 
-    if (vm->nbr_live >= NBR_LIVE) {
-        vm->cycle_to_die -= CYCLE_DELTA;
-        vm->nbr_live = 0;
+static bool programs_alive(vm_t *vm)
+{
+    for (size_t i = 0; i < vm->programs->size; ++i)
+        if (vm->programs->data[i].is_running == true)
+            return true;
+
+    return false;
+}
+
+static void print_winner(vm_t *vm)
+{
+    if (vm->last_live != NULL) {
+        my_printf(
+            "The player %d(%s)has won.\n",
+            vm->last_live->registers[0],
+            vm->last_live->header.prog_name
+        );
     }
 }
 
@@ -64,17 +46,22 @@ void run_vm(vm_t *vm)
 {
     int nb_cycle = 0;
 
-    while (vm->programs->size > 0) {
+    while (programs_alive(vm)) {
         if (nb_cycle == vm->nbr_cycles_to_dump) {
             dump_memory(vm);
             return;
         }
+        if (nb_cycle > 0 && nb_cycle % CYCLE_TO_DIE == 0) {
+            check_alive(vm);
+        }
 
         run_cycle(vm);
 
-        if (nb_cycle > 0 && nb_cycle % CYCLE_TO_DIE == 0)
-            check_alive(vm);
-
+        if (vm->nbr_live >= NBR_LIVE) {
+            vm->cycle_to_die -= CYCLE_DELTA;
+            vm->nbr_live = 0;
+        }
         nb_cycle++;
     }
+    print_winner(vm);
 }
