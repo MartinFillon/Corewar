0f22280c870e6f77f2c40d18d8e55202dc5c7d89
Lou Onezime <lou.onezime@epitech.eu>
2023-05-22 23:57:15 +0200

    fix: :fire: duplicate function
    error with compilation

--- asm/src/labels/labels.c
+++ asm/src/labels/labels.c
@@ -70,23 +70,6 @@ int parse_labels(vec_str_t *lines, asm_t *assembler)
     return SUCCESS;
 }
 
-int parse_labels(vec_str_t *lines, asm_t *assembler)
-{
-    for (size_t i = 0; i < lines->size; i++) {
-        if (lines->data[i]->data[0] == COMMENT_CHAR ||
-            lines->data[i]->length == 0 || lines->data[i]->data[0] == '.') {
-            free(lines->data[i]);
-            vec_remove(lines, i);
-            i--;
-        }
-    }
-    for (size_t i = 0; i < lines->size; i++){
-        if (exclude_instructions(lines->data[i], assembler, lines) == ERROR)
-            return ERROR;
-    }
-    return SUCCESS;
-}
-
 void get_label_value(str_t *label, asm_t *assembler, str_t **buffer)
 {
     long value = 0;
