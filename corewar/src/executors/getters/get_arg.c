0925aefdcb0aaf0bd8ca59c6366173263c7e0183
MartinFillon <martin.fillon@epitech.eu>
2023-05-22 10:19:27 +0200

    fix: :fire: and was shitty coded

---
+++ corewar/src/executors/getters/get_arg.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2023
+** Corewar
+** File description:
+** get_arg
+*/
+
+#include "corewar/corewar.h"
+#include "corewar/op.h"
+
+int get_arg(int *arg, char *memory, int *pc, u_char arg_type)
+{
+    if (arg_type == 0b10) {
+        *arg = get_direct(memory, *pc);
+        *pc = (*pc + DIR_SIZE) % MEM_SIZE;
+        return 1;
+    } else if (arg_type == 0b11) {
+        *arg = get_indirect(memory, *pc, *pc);
+        *pc = (*pc + IND_SIZE) % MEM_SIZE;
+        return 1;
+    } else if (arg_type == 0b01) {
+        *arg = memory[*pc] % (REG_NUMBER + 1);
+        *pc = (*pc + 1) % MEM_SIZE;
+        return 1;
+    } else {
+        return 0;
+    }
+}
