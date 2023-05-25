974112f223774e26b76b2e1391f65fe01e5514ae
Lou Onezime <lou.onezime@epitech.eu>
2023-05-25 12:18:42 +0200

    fix: :bug: calculation errors with static consts
    fixed for large directs

--- asm/src/body/direct_params.c
+++ asm/src/body/direct_params.c
@@ -36,12 +36,12 @@ void get_direct_int(size_t type, long nbr, str_t **buffer)
     if (type == DIR_SIZE) {
         if (nbr < 0) {
             nbr = BITS_DIR_MAX + nbr;
-            str_cadd(buffer, (nbr / (BITS_IND_MAX * BITS_IND_MAX)));
+            str_cadd(buffer, (nbr / (BITS_IND_MAX * MAX_BYTE_VAL)));
             str_cadd(buffer, (nbr / BITS_IND_MAX) % MAX_BYTE_VAL);
             str_cadd(buffer, (nbr / MAX_BYTE_VAL) % MAX_BYTE_VAL);
             str_cadd(buffer, (nbr % MAX_BYTE_VAL));
         } else {
-            str_cadd(buffer, (nbr / (BITS_IND_MAX * BITS_IND_MAX)));
+            str_cadd(buffer, (nbr / (BITS_IND_MAX * MAX_BYTE_VAL)));
             str_cadd(buffer, (nbr / BITS_IND_MAX % MAX_BYTE_VAL));
             str_cadd(buffer, ((nbr / MAX_BYTE_VAL) % MAX_BYTE_VAL));
             str_cadd(buffer, (nbr % MAX_BYTE_VAL));
@@ -55,6 +55,7 @@ int manage_direct(str_t *param, str_t **buffer, size_t type, asm_t *assembler)
     long value = 0;
     str_t *tmp = str_create(param->data + 1);
 
+    printf("===%%%s\n", tmp->data);
     if (tmp->data[0] == LABEL_CHAR){
         get_label_value(tmp, assembler, buffer, type);
         free(tmp);
