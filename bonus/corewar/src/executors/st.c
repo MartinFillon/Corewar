e6db1a7e901efb79bebc26ac04c353faaebff925
MartinFillon <martin.fillon@epitech.eu>
2023-05-26 19:01:45 +0200

    feat: :sparkles: add printing

--- bonus/corewar/src/executors/st.c
+++ bonus/corewar/src/executors/st.c
@@ -18,6 +18,7 @@ int exec_st(vm_t *vm, program_t *p)
     argument_t *args = (argument_t[4]){0};
     ind_state_t ind_state = {vm->arena, p->pc, true};
     int st = p->pc;
+    int write_addr = 0;
 
     get_arg_types(vm->arena, &p->pc, args);
     for (int i = 0; i < 2; ++i) {
@@ -29,9 +30,11 @@ int exec_st(vm_t *vm, program_t *p)
     if (args[1].arg_type == T_REG) {
         p->registers[args[1].data.reg - 1] = get_value(&args[0], p, &ind_state);
     } else {
+        write_addr =(st + get_value(&args[1], p, &ind_state)) % MEM_SIZE;
         write_int(
-            vm->arena, (st + get_value(&args[1], p, &ind_state)) % MEM_SIZE,
+            vm->arena, write_addr,
             get_value(&args[0], p, &ind_state));
+        printf("{name:%s, address:%d, size:%d}\n", p->header.prog_name, write_addr, REG_SIZE);
     }
     return 0;
 }
