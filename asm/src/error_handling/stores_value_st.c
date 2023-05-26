8730e729ee88e8995a8747e6cb56a15cd62ca892
lou <109749395+louonezime@users.noreply.github.com>
2023-05-26 16:06:44 +0200

    fix: :bug: size 0 for st instruction (#96)
    * fix: :bug: size 0 doesnt pass for st instruction
    
    error case is removed
    
    * style: :lipstick: cleanup magic numbers and empty if
    
    coding style

--- asm/src/error_handling/stores_value_st.c
+++ asm/src/error_handling/stores_value_st.c
@@ -18,7 +18,9 @@ int check_st(size_t instruction, vec_str_t *params)
         return ERROR;
     }
     if (params->data[1]->data[0] != 'r' && my_atoi(params->data[1]->data) == 0
-        && params->data[1]->data[0] != '0'){
+        && params->data[1]->data[0] != '0' &&
+        params->data[1]->data[0] != LABEL_CHAR &&
+        OP_NAME[instruction].size != 2){
         my_dprintf(2, "%s: takes a register or a number as 2nd parameter\n",
         OP_NAME[instruction].name);
         return ERROR;
